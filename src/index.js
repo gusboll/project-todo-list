70c21f17bb45533da1d481598af2fa051172c351
gusboll <augusto.sboll@gmail.com>
2026-01-29 12:37:38 -0300

    add finer html, css and began js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
+import { title } from 'process';
 import './style.css';
+
+
 console.log("Funcionando");
+
+const homeUserButton = document.getElementById("homeUserButton");
+const addButton = document.getElementById("addButton");
+const todayButton = document.getElementById("todayButton");
+const calendarButton = document.getElementById("calendarButton");
+const tasksButton = document.getElementById("tasksButton");
+
+homeUserButton.addEventListener("click", () =>{
+    console.log("homeUserButton clicked")
+})
+
+addButton.addEventListener("click", () =>{
+    console.log("addButton clicked")
+})
+
+todayButton.addEventListener("click", () =>{
+    console.log("todayButton clicked")
+})
+
+calendarButton.addEventListener("click", () =>{
+    console.log("calendarButton clicked")
+})
+
+tasksButton.addEventListener("click", () =>{
+    console.log("tasksButton clicked")
+})
+
+// todo items need to have:
+
+// title; description; dueDate; priority; notes/checklist
+// should be able to create different projects/todos
+// should be able to: view all projects; view all todos in each project; expand a single todo; delete a todo;
+// it needs to integrate dates and the calendar
+// needs to save the data on local user computer
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,15 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext]'
+                }
+            },
+
             {
                 test: /\.html$/i,
                 loader: "html-loader",
